275367a1bf50465210508f538e3ed933bf73dc3b
artkulak <artkulak444@gmail.com>
2023-09-13 12:54:28 +0000

    update py sdk

--- vocode/streaming/agent/chat_gpt_agent.py
+++ vocode/streaming/agent/chat_gpt_agent.py
@@ -115,7 +115,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
         conversation_id: str,
         is_interrupt: bool = False,
     ) -> Tuple[str, bool]:
-        self.logger.debug('respond ' + human_input + ' ' + is_interrupt)
+        self.logger.debug('respond ' + human_input + ' ' + str(is_interrupt))
         assert self.transcript is not None
         if is_interrupt and self.agent_config.cut_off_response:
             cut_off_response = self.get_cut_off_response()
@@ -138,7 +138,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
         conversation_id: str,
         is_interrupt: bool = False,
     ) -> AsyncGenerator[Tuple[Union[str, FunctionCall], bool], None]:
-        self.logger.debug('generate_response ' + human_input + ' ' + is_interrupt)
+        self.logger.debug('generate_response ' + human_input + ' ' + str(is_interrupt))
         if is_interrupt and self.agent_config.cut_off_response:
             cut_off_response = self.get_cut_off_response()
             yield cut_off_response, False
