196a42e63de6e7c0d0612e9b125628767c238f7e
artkulak <artkulak444@gmail.com>
2023-10-24 20:26:27 +0000

    update py sdk

--- vocode/streaming/models/client_backend.py
+++ vocode/streaming/models/client_backend.py
@@ -15,14 +15,14 @@ class OutputAudioConfig(BaseModel):
     audio_encoding: AudioEncoding
     
 class ConversationData(BaseModel):
-    user_id: str,
-    user_first_name: str,
-    user_last_name: str,
-    user_interests: list[str],
-    deeva_profile_id: str,
-    deeva_memory: str,
-    deeva_name: str,
-    deeva_relationship_type: str,
+    user_id: str
+    user_first_name: str
+    user_last_name: str
+    user_interests: list[str]
+    deeva_profile_id: str
+    deeva_memory: str
+    deeva_name: str
+    deeva_relationship_type: str
     deeva_interests: list[str]
     
     
